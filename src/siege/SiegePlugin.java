31f2b6cc391e8d78ee277298ce2a553362a1f6d7
nilederg <nilederg@tutanota.com>
2024-10-12 21:13:12 -0700

    Create main file

---
+++ src/siege/SiegePlugin.java
@@ -0,0 +1,14 @@
+package siege;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.net.Administration.*;
+import mindustry.world.blocks.storage.*;
+
+public class SiegePlugin extends Plugin {
+}
