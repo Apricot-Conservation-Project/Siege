2ecfa82387f2d590301c42f3763146081f63f38f
nilederg <nilederg@tutanota.com>
2024-10-13 22:43:06 -0700

    Improve build script

--- make_mod.sh
+++ make_mod.sh
@@ -1,6 +1,8 @@
 set -e
 echo Making jar
-./gradlew jar
-echo Copying
+./gradlew clean jar
+echo Clearing mods folder
+rm ../mserv/config/mods/* || echo "Mods folder already empty"
+echo Copying jar
 cp ./build/libs/* ../mserv/config/mods/
 
