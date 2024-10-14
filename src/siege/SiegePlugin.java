530516b82b851bf7266091301c674bbb4726abc3
nilederg <nilederg@tutanota.com>
2024-10-14 02:58:46 -0700

    Add game initialization, wrappers for commands and updates, and handle empty teams

--- src/siege/SiegePlugin.java
+++ src/siege/SiegePlugin.java
@@ -4,11 +4,102 @@ import arc.*;
 import arc.util.*;
 import mindustry.*;
 import mindustry.content.*;
+import mindustry.core.GameState;
+import mindustry.game.EventType;
 import mindustry.game.EventType.*;
 import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.net.Administration.*;
 import mindustry.world.blocks.storage.*;
 
+import java.util.Set;
+
 public class SiegePlugin extends Plugin {
+    public static Gamedata gamedata;
+    public static final int SETUP_TIME_SECONDS = 3 * 60;
+
+    @Override
+    public void init() {
+        System.out.println("SiegePlugin loaded");
+
+        gamedata = new Gamedata();
+
+        Events.run(EventType.Trigger.update, () -> {
+            alwaysUpdate();
+
+            if (!gamedata.gameStarted()) {
+                Setup.update();
+            } else {
+                gameUpdate();
+            }
+        });
+    }
+
+    /**
+     * Ends the current game and loads the next.
+     * @param winner The game's winner. 0 if the Citadel wins, otherwise is the ID of the winning team. -1 if the game is ended without a winner.
+     */
+    public void endGame(int winner) {
+        switch (winner) {
+            // ???
+        }
+    }
+
+    // Manages constant processes that happen always
+    private void alwaysUpdate() {
+        //
+    }
+
+    // Manages constant processes after setup
+    private void gameUpdate() {
+        //
+    }
+
+    private void checkTeams() {
+        for (RaiderTeam team : gamedata.raiderTeams) {
+            if (team.players.isEmpty()) {
+                gamedata.raiderTeams.remove(team);
+            }
+        }
+
+        if (gamedata.raiderTeams.isEmpty()) {
+            endGame(0);
+        }
+    }
+
+    @Override
+    public void registerClientCommands(CommandHandler handler) {
+        RaiderTeam.Commands.registerCommands(handler);
+    }
+
+    @Override
+    public void registerServerCommands(CommandHandler handler) {
+        /*base.register_server(handler);
+        handler.removeCommand("host");
+        handler.removeCommand("gameover");
+        handler.removeCommand("runwave");
+        handler.removeCommand("shuffle");
+        handler.removeCommand("nextmap");
+        handler.register("host", "[map(index)]", "Host the Siege game mode", args -> {
+            if (!Vars.state.is(GameState.State.menu)) {
+                Log.err("Stop the server first.");
+                return;
+            }
+            mapReset(args);
+        });
+
+        handler.register("gameover", "[map(index)]", "End the Siege game", args -> {
+            Call.sendMessage("[scarlet]Server[accent] has ended the plague game. Ending in 10 seconds...");
+            Log.info("Game ended.");
+            // TODO end the game
+        });*/
+    }
+
+
+
+    public static void announce(String message) {
+        for (Player player : Groups.player) {
+            player.sendMessage(message);
+        }
+    }
 }
