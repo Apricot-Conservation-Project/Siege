34322f6a601de5b281e7424f42a9985cd871a333
nilederg <nilederg@gmail.com>
2025-04-16 18:18:54 -0700

    Simplify java usage

--- build-run.sh
+++ build-run.sh
@@ -1,4 +1,14 @@
+cd ~/Git/Siege-Environment/Siege
 bash make_mod.sh
 echo ""
 cd ../mserv
-java -jar server-release.jar
+
+JAVA_HOME=$(find /usr/lib/jvm -maxdepth 1 -type d -printf "%f\n" | grep 17)
+
+if [ -n "$JAVA_HOME" ]; then
+    JAVA="/usr/lib/jvm/$JAVA_HOME/bin/java"
+    $JAVA -jar server-release.jar
+else
+    echo "Java 17 not set up. Please install jdk17-openjdk."
+    exit 1
+fi
