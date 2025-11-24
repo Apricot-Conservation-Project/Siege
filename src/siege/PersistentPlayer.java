1600a68a78fbfc0cf02f44f14686beaa3945a357
nilederg <nilederg@gmail.com>
2025-11-24 13:45:51 -0800

    Fix click action

--- src/siege/PersistentPlayer.java
+++ src/siege/PersistentPlayer.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Objects;
 
 public class PersistentPlayer {
-    public ClickAction clickAction;
+    public ClickAction clickAction = ClickAction.None;
     public boolean online;
     public long lastSeen;
     public long lastActed;
